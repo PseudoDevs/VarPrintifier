b5efa4142943d82d7d7db724e1ef627428984cac
PseudoDevs <johnlester.mychannelnow@gmail.com>
2023-04-20 23:07:54 +0800

    Deploying for Dev

--- src/VarPrintifier.php
+++ src/VarPrintifier.php
@@ -4,30 +4,59 @@ namespace PHPVarPrintifier;
 
 class VarPrintifier
 {
-    public static function print($value, $func = 'var_export', $height = '300px', $width = '100%', $theme = 'dracula', $exit = true) {
-        $editorId = $func . '_editor';
+    public static function print($value, $func = 'var_export', $exit = true) {
+        $code = htmlspecialchars($func($value ?? '', true));
 
-        // Create the initial HTML for the Ace editor
-        echo sprintf('<div id="%s" style="height: %s; width: %s;"></div>', $editorId, $height, $width);
-        echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.min.js"></script>';
+        // Custom syntax highlighting styles
+        $css = '
+            <style>
+            pre {
+                background-color: #2f3640;
+                color: #fff;
+                padding: 1rem;
+                font-family: monospace;
+                font-size: 14px;
+                line-height: 1.5;
+            }
+                .vp-keyword { color: #6DC6FF; }
+                .vp-string { color: #A8FF60; }
+                .vp-number { color: #FFB977; }
+                .vp-comment { color: #7F848E; font-style: italic; }
+            </style>
+        ';
 
-        // Get the current editor instance (or create a new one if it doesn't exist)
-        echo sprintf('<script>var %s = ace.edit("%s");</script>', $editorId, $editorId);
-        echo sprintf('<script>%s.setTheme("ace/theme/%s");</script>', $editorId, $theme);
-        echo sprintf('<script>%s.session.setMode("ace/mode/javascript");</script>', $editorId);
-        echo sprintf('<script>%s.setReadOnly(true);</script>', $editorId);
-        echo sprintf('<script>%s.setValue(%s);</script>', $editorId, json_encode($func($value, true)));
+        // Custom syntax highlighting patterns and their corresponding callback functions
+        $patterns = [
+            '/(?<!\\\\)(["\'])(?:\\\\.|(?!\1).)*\1/' => function ($match) {
+                return '<span class="vp-string">' . $match[0] . '</span>';
+            }, // Strings
+            '/(?<!\w|-)([0-9][0-9\.e+-]*)/' => function ($match) {
+                return '<span class="vp-number">' . $match[0] . '</span>';
+            }, // Numbers
+            '/(=>|\bArray\b|\bbool\b|\bboolean\b|\bnull\b|\bNULL\b|\bint\b|\binteger\b|\bfloat\b|\bdouble\b|\bstring\b|\bresource\b|\bobject\b|\bthis\b|\bself::\b)/' => function ($match) {
+                return '<span class="vp-keyword">' . $match[0] . '</span>';
+            }, // Keywords
+            '/#.*$/m' => function ($match) {
+                return '<span class="vp-comment">' . $match[0] . '</span>';
+            }, // Comments
+        ];
+
+        // Apply syntax highlighting
+        $code = preg_replace_callback_array($patterns, $code);
+
+        // Output the styles and HTML
+        echo $css . '<pre>' . $code . '</pre>';
 
         if ($exit) {
             exit();
         }
     }
 
-    public static function var_dump($value, $height = '300px', $width = '100%', $theme = 'dracula', $exit = true) {
-        self::print($value, 'var_dump', $height, $width, $theme, $exit);
+    public static function var_dump($value, $exit = true) {
+        self::print($value, 'var_dump', $exit);
     }
 
-    public static function print_r($value, $height = '300px', $width = '100%', $theme = 'dracula', $exit = true) {
-        self::print($value, 'print_r', $height, $width, $theme, $exit);
+    public static function print_r($value, $exit = true) {
+        self::print($value, 'print_r', $exit);
     }
 }
