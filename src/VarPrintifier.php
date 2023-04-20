4694ad697284bc8ad597428eb68e6560ad781026
PseudoDevs <johnlester.mychannelnow@gmail.com>
2023-04-20 22:03:01 +0800

    Rebuilding the structure of the print()

--- src/VarPrintifier.php
+++ src/VarPrintifier.php
@@ -4,29 +4,30 @@ namespace PHPVarPrintifier;
 
 class VarPrintifier
 {
-    public static function dump($value, $exit = true) {
-        $editorId = 'dump-editor';
-        $editorHeight = '300px';
-        $editorWidth = '100%';
+    public static function print($value, $func = 'var_export', $height = '300px', $width = '100%', $theme = 'dracula', $exit = true) {
+        $editorId = $func . '_editor';
 
-        echo sprintf('<div id="%s" style="height: %s; width: %s;"></div>', $editorId, $editorHeight, $editorWidth);
-        echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js" integrity="sha512-VVAeqwJF1Q4DPRzZZL9Q2IKgE0WmB3D/A+HITb71PhEDAZjK+BZc8dkRlhzX9M9IMjcE30h7y8RvOj6/v2U5ag==" crossorigin="anonymous"></script>';
-        echo sprintf('<script>var %s = ace.edit("%s"); %s.setTheme("ace/theme/chrome"); %s.session.setMode("ace/mode/php"); %s.setReadOnly(true); %s.setValue(%s);</script>', $editorId, $editorId, $editorId, $editorId, $editorId, $editorId, json_encode(var_export($value, true)));
+        // Create the initial HTML for the Ace editor
+        echo sprintf('<div id="%s" style="height: %s; width: %s;"></div>', $editorId, $height, $width);
+        echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.min.js"></script>';
+
+        // Get the current editor instance (or create a new one if it doesn't exist)
+        echo sprintf('<script>var %s = ace.edit("%s");</script>', $editorId, $editorId);
+        echo sprintf('<script>%s.setTheme("ace/theme/%s");</script>', $editorId, $theme);
+        echo sprintf('<script>%s.session.setMode("ace/mode/javascript");</script>', $editorId);
+        echo sprintf('<script>%s.setReadOnly(true);</script>', $editorId);
+        echo sprintf('<script>%s.setValue(%s);</script>', $editorId, json_encode($func($value, true)));
+
         if ($exit) {
             exit();
         }
     }
 
-    public static function printR($value, $exit = true) {
-        $editorId = 'printr-editor';
-        $editorHeight = '300px';
-        $editorWidth = '100%';
+    public static function var_dump($value, $height = '300px', $width = '100%', $theme = 'dracula', $exit = true) {
+        self::print($value, 'var_dump', $height, $width, $theme, $exit);
+    }
 
-        echo sprintf('<div id="%s" style="height: %s; width: %s;"></div>', $editorId, $editorHeight, $editorWidth);
-        echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ace.js" integrity="sha512-VVAeqwJF1Q4DPRzZZL9Q2IKgE0WmB3D/A+HITb71PhEDAZjK+BZc8dkRlhzX9M9IMjcE30h7y8RvOj6/v2U5ag==" crossorigin="anonymous"></script>';
-        echo sprintf('<script>var %s = ace.edit("%s"); %s.setTheme("ace/theme/chrome"); %s.session.setMode("ace/mode/php"); %s.setReadOnly(true); %s.setValue(%s);</script>', $editorId, $editorId, $editorId, $editorId, $editorId, $editorId, json_encode(print_r($value, true)));
-        if ($exit) {
-            exit();
-        }
+    public static function print_r($value, $height = '300px', $width = '100%', $theme = 'dracula', $exit = true) {
+        self::print($value, 'print_r', $height, $width, $theme, $exit);
     }
 }
